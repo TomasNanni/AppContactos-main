3a88ed998807bd65b841977e2a4cee844cf78293
TomasNanni <tomasnanni18@gmail.com>
2025-10-04 19:18:20 -0300

    agregado la revision de token

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,4 +1,4 @@
-import { inject , Injectable } from '@angular/core';
+import { inject, Injectable, OnInit } from '@angular/core';
 import { LoginData } from '../interfaces/auth';
 import { Router } from '@angular/router';
 
@@ -6,32 +6,60 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 
-export class Auth {
+export class Auth implements OnInit {
 
-  token: null|string = localStorage.getItem("token");
+  ngOnInit(): void {
+    // Si tengo sesion iniciada reviso que no este vencida
+    if (this.token) {
+      this.revisionTokenInterval = this.revisionToken()
+    }
+  }
+
+  token: null | string = localStorage.getItem("token");
   router = inject(Router);
+  revisionTokenInterval: number | undefined;
 
-  async login(loginData: LoginData){
+  async login(loginData: LoginData) {
     const res = await fetch('https://agenda-api.somee.com/api/authentication/authenticate',
       {
-        method:"POST",
+        method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
         body: JSON.stringify(loginData),
       }
     )
-    if(res.ok){
+    if (res.ok) {
       const resText = await res.text()
       this.token = resText;
-      localStorage.setItem("token",this.token);
+      localStorage.setItem("token", this.token);
+      this.revisionTokenInterval = this.revisionToken()
     }
     return res.ok;
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem("token");
     this.token = null;
     this.router.navigate(["/"]);
+    if(this.revisionTokenInterval) clearInterval(this.revisionTokenInterval);
+  }
+
+    /** Revisa cada 10 minutos que el token siga siendo valido */
+  revisionToken() {
+    return setInterval(() => {
+      if (this.token) {
+        const base64Url = this.token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        const claims: { exp: number } = JSON.parse(jsonPayload);
+        if (new Date(claims.exp * 1000) < new Date()) {
+          this.logout()
+        }
+      }
+    }, 600)
   }
 }
