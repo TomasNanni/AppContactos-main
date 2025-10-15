7dfcfa66466ae464a562e266440ac486e57be2b7
TomasNanni <tomasnanni18@gmail.com>
2025-10-15 16:19:36 -0300

    terminar el spinner y block en todos los lugares

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -3,17 +3,18 @@ import { Router, RouterModule } from '@angular/router';
 import { Auth } from '../../services/auth-service';
 import { FormsModule, NgForm } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { Spinner } from '../../components/spinner/spinner';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, Spinner],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss'
 })
 export class LoginPage {
   authService = inject(Auth)
   router = inject(Router)
-
+  solicitudABackEnCurso = false;
   errorLogin = false;
 
   async login(form: NgForm) {
@@ -22,15 +23,12 @@ export class LoginPage {
       this.errorLogin = true;
       return
     }
+    this.solicitudABackEnCurso = true;
+
     const loginResult = await this.authService.login(form.value);
+    this.solicitudABackEnCurso = false;
+
     if (loginResult) this.router.navigate(["/logged/contact-list-page"]);
     this.errorLogin = true;
-  }
-
-  showErrorToastMail() {
-    Swal.fire({
-      title: "Error",
-      text: "Debe colocar un correo electrónico"
-    });
   }
 }
